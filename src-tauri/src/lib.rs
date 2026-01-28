2a4b3753cb0361055a6d5c5fec96a74924c9f31a
hystericca <frankvluna@proton.me>
2026-01-27 22:05:16 -0800

    initial commit amend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,6 +5,7 @@ mod cmd;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        // Add commands here
         .invoke_handler(tauri::generate_handler![
             cmd::greet,
             cmd::my_custom_command
