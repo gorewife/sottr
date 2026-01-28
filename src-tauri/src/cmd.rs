45d207b78343c3efd88432c08b63ee09b0fc4b73
hystericca <frankvluna@proton.me>
2026-01-27 22:04:13 -0800

    initial commit

---
+++ src-tauri/src/cmd.rs
@@ -0,0 +1,9 @@
+#[tauri::command]
+pub fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+pub fn my_custom_command() {
+    println!("Greetings from JavaScript!");
+}
