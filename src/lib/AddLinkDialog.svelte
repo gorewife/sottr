9fe75be64b55a2f525be6cbeb414fdbf23c24255
hystericca <frankvluna@proton.me>
2026-01-28 01:53:12 -0800

    add: link dialog component

---
+++ src/lib/AddLinkDialog.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  let { open, onSubmit } = $props<{
+    open: boolean;
+    onSubmit: (link: string) => void;
+  }>();
+
+  let link = $state("");
+</script>
+
+{#if open}
+  <div class="dialog">
+    <h2>Add link</h2>
+
+    <input
+      type="url"
+      bind:value={link}
+    />
+
+    <div class="actions">
+      <button onclick={() => (open = false)}>Cancel</button>
+      <button
+        onclick={() => {
+          onSubmit(link);
+          open = false;
+          link = "";
+        }}
+        disabled={!link}
+      >
+        Add
+      </button>
+    </div>
+  </div>
+{/if}
